ef0fb70abe3c17f8f5844856223163e78000c6f9
rajaabdullahnasir <rajaabdullahnasir19@gmail.com>
2026-09-14 01:55:11 +0500

    last build

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@
  * SPDX-License-Identifier: Apache-2.0
  */
 
-import { useEffect } from 'react';
+import { Suspense, lazy, useEffect } from 'react';
 import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from 'react-router-dom';
 import Navbar from './components/Navbar';
 import Hero from './components/Hero';
@@ -16,32 +16,44 @@ import PCATeaser from './components/PCATeaser';
 import TrustedBy from './components/TrustedBy';
 import PartnersSlider from './components/PartnersSlider';
 import Footer from './components/Footer';
-import AboutPage from './pages/AboutPage';
-import ContactPage from './pages/ContactPage';
-import PCAPage from './pages/PCAPage';
-import MyesiLoginPage from './pages/MyesiLoginPage';
-import LegalPage from './pages/LegalPage';
-import PrivacyPolicyPage from './pages/PrivacyPolicyPage';
-import TermsOfServicePage from './pages/TermsOfServicePage';
-import CookiePolicyPage from './pages/CookiePolicyPage';
-import DemoPage from './pages/DemoPage';
-import AllReportsPage from './pages/AllReportsPage';
-import CompliancePage from './pages/CompliancePage';
-import ServicesPage from './pages/ServicesPage';
-import BecomePartnerPage from './pages/partners/BecomePartnerPage';
-import FindPartnerPage from './pages/partners/FindPartnerPage';
-import PartnerPortalPage from './pages/partners/PartnerPortalPage';
-import PartnerProgramsPage from './pages/partners/PartnerProgramsPage';
-import TrainingPage from './pages/resources/TrainingPage';
-import DocumentationPage from './pages/resources/DocumentationPage';
-import CommunityPage from './pages/resources/CommunityPage';
-import TrustPage from './pages/resources/TrustPage';
-import ExecutiveTeamPage from './pages/company/ExecutiveTeamPage';
-import InvestorRelationsPage from './pages/company/InvestorRelationsPage';
-import CareersPage from './pages/CareersPage';
-import ServiceDetailPage from './pages/ServiceDetailPage';
-import TrainingDetailPage from './pages/TrainingDetailPage';
-import ComparePlansPage from './pages/partners/ComparePlansPage';
+
+// Every page below is loaded on demand (code-split) instead of all at once,
+// so visiting the homepage doesn't download the code for every other page
+// on the site. This is what fixes the "chunk larger than 500kB" build warning.
+const AboutPage = lazy(() => import('./pages/AboutPage'));
+const ContactPage = lazy(() => import('./pages/ContactPage'));
+const PCAPage = lazy(() => import('./pages/PCAPage'));
+const MyesiLoginPage = lazy(() => import('./pages/MyesiLoginPage'));
+const LegalPage = lazy(() => import('./pages/LegalPage'));
+const PrivacyPolicyPage = lazy(() => import('./pages/PrivacyPolicyPage'));
+const TermsOfServicePage = lazy(() => import('./pages/TermsOfServicePage'));
+const CookiePolicyPage = lazy(() => import('./pages/CookiePolicyPage'));
+const DemoPage = lazy(() => import('./pages/DemoPage'));
+const AllReportsPage = lazy(() => import('./pages/AllReportsPage'));
+const CompliancePage = lazy(() => import('./pages/CompliancePage'));
+const ServicesPage = lazy(() => import('./pages/ServicesPage'));
+const BecomePartnerPage = lazy(() => import('./pages/partners/BecomePartnerPage'));
+const FindPartnerPage = lazy(() => import('./pages/partners/FindPartnerPage'));
+const PartnerPortalPage = lazy(() => import('./pages/partners/PartnerPortalPage'));
+const PartnerProgramsPage = lazy(() => import('./pages/partners/PartnerProgramsPage'));
+const TrainingPage = lazy(() => import('./pages/resources/TrainingPage'));
+const DocumentationPage = lazy(() => import('./pages/resources/DocumentationPage'));
+const CommunityPage = lazy(() => import('./pages/resources/CommunityPage'));
+const TrustPage = lazy(() => import('./pages/resources/TrustPage'));
+const ExecutiveTeamPage = lazy(() => import('./pages/company/ExecutiveTeamPage'));
+const InvestorRelationsPage = lazy(() => import('./pages/company/InvestorRelationsPage'));
+const CareersPage = lazy(() => import('./pages/CareersPage'));
+const ServiceDetailPage = lazy(() => import('./pages/ServiceDetailPage'));
+const TrainingDetailPage = lazy(() => import('./pages/TrainingDetailPage'));
+const ComparePlansPage = lazy(() => import('./pages/partners/ComparePlansPage'));
+
+function PageLoader() {
+  return (
+    <div className="min-h-screen flex items-center justify-center">
+      <div className="w-8 h-8 border-3 border-teal-500 border-t-transparent rounded-full animate-spin" />
+    </div>
+  );
+}
 
 function ScrollToHash() {
   const { hash } = useLocation();
@@ -93,39 +105,41 @@ export default function App() {
         <div className="relative z-10">
           <Navbar />
           <main>
-            <Routes>
-              <Route path="/" element={<Home />} />
-              <Route path="/about" element={<AboutPage />} />
-              <Route path="/myesi" element={<Navigate to="/#product" replace />} />
-              <Route path="/features" element={<Navigate to="/#features" replace />} />
-              <Route path="/packages" element={<Navigate to="/#packages" replace />} />
-              <Route path="/support" element={<Navigate to="/#support" replace />} />
-              <Route path="/contact" element={<ContactPage />} />
-              <Route path="/pca" element={<PCAPage />} />
-              <Route path="/myesi/login" element={<MyesiLoginPage />} />
-              <Route path="/legal" element={<LegalPage />} />
-              <Route path="/privacy" element={<PrivacyPolicyPage />} />
-              <Route path="/terms" element={<TermsOfServicePage />} />
-              <Route path="/cookies" element={<CookiePolicyPage />} />
-              <Route path="/demo" element={<DemoPage />} />
-              <Route path="/reports" element={<AllReportsPage />} />
-              <Route path="/compliance/:id" element={<CompliancePage />} />
-              <Route path="/services" element={<ServicesPage />} />
-              <Route path="/services/:slug" element={<ServiceDetailPage />} />
-              <Route path="/careers" element={<CareersPage />} />
-              <Route path="/partners/become-a-partner" element={<BecomePartnerPage />} />
-              <Route path="/partners/find-a-partner" element={<FindPartnerPage />} />
-              <Route path="/partners/portal" element={<PartnerPortalPage />} />
-              <Route path="/partners/programs" element={<PartnerProgramsPage />} />
-              <Route path="/partners/compare-plans" element={<ComparePlansPage />} />
-              <Route path="/resources/training" element={<TrainingPage />} />
-              <Route path="/resources/training/:slug" element={<TrainingDetailPage />} />
-              <Route path="/resources/documentation" element={<DocumentationPage />} />
-              <Route path="/resources/community" element={<CommunityPage />} />
-              <Route path="/resources/trust" element={<TrustPage />} />
-              <Route path="/company/executive-team" element={<ExecutiveTeamPage />} />
-              <Route path="/company/investor-relations" element={<InvestorRelationsPage />} />
-            </Routes>
+            <Suspense fallback={<PageLoader />}>
+              <Routes>
+                <Route path="/" element={<Home />} />
+                <Route path="/about" element={<AboutPage />} />
+                <Route path="/myesi" element={<Navigate to="/#product" replace />} />
+                <Route path="/features" element={<Navigate to="/#features" replace />} />
+                <Route path="/packages" element={<Navigate to="/#packages" replace />} />
+                <Route path="/support" element={<Navigate to="/#support" replace />} />
+                <Route path="/contact" element={<ContactPage />} />
+                <Route path="/pca" element={<PCAPage />} />
+                <Route path="/myesi/login" element={<MyesiLoginPage />} />
+                <Route path="/legal" element={<LegalPage />} />
+                <Route path="/privacy" element={<PrivacyPolicyPage />} />
+                <Route path="/terms" element={<TermsOfServicePage />} />
+                <Route path="/cookies" element={<CookiePolicyPage />} />
+                <Route path="/demo" element={<DemoPage />} />
+                <Route path="/reports" element={<AllReportsPage />} />
+                <Route path="/compliance/:id" element={<CompliancePage />} />
+                <Route path="/services" element={<ServicesPage />} />
+                <Route path="/services/:slug" element={<ServiceDetailPage />} />
+                <Route path="/careers" element={<CareersPage />} />
+                <Route path="/partners/become-a-partner" element={<BecomePartnerPage />} />
+                <Route path="/partners/find-a-partner" element={<FindPartnerPage />} />
+                <Route path="/partners/portal" element={<PartnerPortalPage />} />
+                <Route path="/partners/programs" element={<PartnerProgramsPage />} />
+                <Route path="/partners/compare-plans" element={<ComparePlansPage />} />
+                <Route path="/resources/training" element={<TrainingPage />} />
+                <Route path="/resources/training/:slug" element={<TrainingDetailPage />} />
+                <Route path="/resources/documentation" element={<DocumentationPage />} />
+                <Route path="/resources/community" element={<CommunityPage />} />
+                <Route path="/resources/trust" element={<TrustPage />} />
+                <Route path="/company/executive-team" element={<ExecutiveTeamPage />} />
+                <Route path="/company/investor-relations" element={<InvestorRelationsPage />} />
+              </Routes>
+            </Suspense>
           </main>
           <Footer />
         </div>
